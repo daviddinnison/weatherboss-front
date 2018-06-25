0c83e6a888a28baf38ea4f3542ed6edb8432c891
daviddinnison <david.g.dinnison@gmail.com>
2018-06-25 11:25:23 -0400

    refactor

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
-export const API_BASE_URL =
-  process.env.REACT_APP_API_BASEURL || "http://localhost:8080/api";
-export const WUNDERGROUND_API_KEY = process.env.WUNDERGROUND_API_KEY || 'b20a7be72cb0b77a';
+module.exports = {
+  API_BASE_URL: process.env.REACT_APP_API_BASEURL || 'http://localhost:8080/api',
+  WUNDERGROUND_API_KEY: process.env.REACT_APP_WUNDERGROUND_API_KEY
+};
